c074e765f21ad967bf09b884575f2e840c4c00d4
Lata <prajapatilata01@gmail.com>
2024-07-23 13:35:51 +0530

    add

--- Lab Assignment-2/3.py
+++ Lab Assignment-2/3.py
@@ -1,1 +1,13 @@
-# Write a Python program that opens a file and handle FileNotFoundError exception if file does not exist
+# Write a Python program that opens a file and handle FileNotFoundError exception if file does not exist.
+
+def read_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
+            print(content)
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path}' does not exist.")
+
+# Example usage
+file_path = 'example.txt'
+read_file(file_path)
--- Lab Assignment-2/4.py
+++ Lab Assignment-2/4.py
@@ -1,1 +1,18 @@
 # Write a Python program that prompts the user to input two number and raises a TypeError exception if the input are not  numerical
+
+def get_number(prompt):
+    user_input = input(prompt)
+    if not user_input.isdigit():
+        raise TypeError(f"Error: '{user_input}' is not a numerical input.")
+    return int(user_input)
+
+def main():
+    try:
+        num1 = get_number("Enter thje first number: ")
+        num2 = get_number("Enter the second number: ")
+        print(f"The numbers you entered are {num1} and {num2}.")
+    except TypeError as e:
+        print(e)
+
+if __name__ == "__main__":
+    main()
