8bac0649d970446b367ee5f19e1bd71700a7597b
Lata <prajapatilata01@gmail.com>
2024-07-29 15:23:54 +0530

    add

--- Numpy Lab Assignment-1/3.2.py
+++
@@ -1,8 +0,0 @@
-# Write a numpy program to create a 3*3 matrix with values ranging from 2 to 10.
-
-import numpy as np
-
-matrix = np.arange(2, 11).reshape(3, 3)
-
-# Print the matrix
-print(matrix)
