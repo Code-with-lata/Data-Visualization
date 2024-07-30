49aa954d652a0dcd2a013ffe2a8e5ae5de6cb0e8
Lata <prajapatilata01@gmail.com>
2024-07-30 23:00:57 +0530

    add

--- Lab Assignment-2/4.py
+++ Lab Assignment-2/4.py
@@ -8,7 +8,7 @@ def get_number(prompt):
 
 def main():
     try:
-        num1 = get_number("Enter thje first number: ")
+        num1 = get_number("Enter the first number: ")
         num2 = get_number("Enter the second number: ")
         print(f"The numbers you entered are {num1} and {num2}.")
     except TypeError as e:
